5004ef7c61703a9055debde4ece4ca91996492b9
ILAM VENKATA SATWIK <124766612+Ilam-venkata-Satwik@users.noreply.github.com>
2025-06-27 15:17:12 +0530

    Add files via upload

---
+++ JavaFSE-DeepSkilling-Solutions/Week1_DesignPatternsAndPrinciples/ProxyPatternExample/src/Main.java
@@ -0,0 +1,11 @@
+ class Main {
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test.jpg");
+
+        System.out.println("First call:");
+        image.display();
+
+        System.out.println("Second call:");
+        image.display();
+    }
+}
