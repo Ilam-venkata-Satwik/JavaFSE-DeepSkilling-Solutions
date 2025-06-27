a6d2eee20990de767b3c9bab36174d32c4ac74aa
ILAM VENKATA SATWIK <124766612+Ilam-venkata-Satwik@users.noreply.github.com>
2025-06-27 15:07:36 +0530

    Add files via upload

---
+++ JavaFSE-DeepSkilling-Solutions/Week1_DesignPatternsAndPrinciples/FactoryMethodPatternExample/src/Main.java
@@ -0,0 +1,7 @@
+ class Main {
+    public static void main(String[] args) {
+        DocumentFactory factory = new PdfFactory();
+        Document document = factory.createDocument();
+        document.open();
+    }
+}
