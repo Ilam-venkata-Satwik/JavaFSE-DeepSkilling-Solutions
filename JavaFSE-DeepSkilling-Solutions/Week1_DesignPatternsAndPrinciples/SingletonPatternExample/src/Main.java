5679b13987e865e2f154e71168b522fd8e382c1f
ILAM VENKATA SATWIK <124766612+Ilam-venkata-Satwik@users.noreply.github.com>
2025-06-27 15:19:22 +0530

    Add files via upload

---
+++ JavaFSE-DeepSkilling-Solutions/Week1_DesignPatternsAndPrinciples/SingletonPatternExample/src/Main.java
@@ -0,0 +1,11 @@
+ class Main {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("First Message");
+        logger2.log("Second Message");
+
+        System.out.println("Same instance? " + (logger1 == logger2));
+    }
+}
