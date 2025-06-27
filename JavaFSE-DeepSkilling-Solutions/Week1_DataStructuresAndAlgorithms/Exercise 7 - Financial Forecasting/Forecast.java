a802dc69dd02190fdda4cbf7525046b96666bb61
ILAM VENKATA SATWIK <124766612+Ilam-venkata-Satwik@users.noreply.github.com>
2025-06-27 14:37:23 +0530

    Add files via upload

---
+++ JavaFSE-DeepSkilling-Solutions/Week1_DataStructuresAndAlgorithms/Exercise 7 - Financial Forecasting/Forecast.java
@@ -0,0 +1,6 @@
+public class Forecast {
+    public static double forecast(double amount, double rate, int years) {
+        if (years == 0) return amount;
+        return forecast(amount * (1 + rate), rate, years - 1);
+    }
+}
---
+++ JavaFSE-DeepSkilling-Solutions/Week1_DataStructuresAndAlgorithms/Exercise 7 - Financial Forecasting/Main.java
@@ -0,0 +1,6 @@
+ class Main {
+    public static void main(String[] args) {
+        double result = Forecast.forecast(1000, 0.1, 5);
+        System.out.println("Future Value: " + result);
+    }
+}
